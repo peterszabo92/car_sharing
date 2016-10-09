f1661601b87b6fe2d6ec7e8dc36c4f7706643745
Peter Szabo <peter.szabo@hipteam.io>
2016-10-09 22:19:31 +0200

    Helymeghatározáshoz engedély logika Android Marshmallow feletti verziókhoz

--- CarSharing/app/src/main/java/hu/szakdolgozat/carsharing/activity/SplashActivity.java
+++ CarSharing/app/src/main/java/hu/szakdolgozat/carsharing/activity/SplashActivity.java
@@ -1,9 +1,17 @@
 package hu.szakdolgozat.carsharing.activity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import hu.szakdolgozat.carsharing.R;
 
@@ -12,11 +20,20 @@ import hu.szakdolgozat.carsharing.R;
  */
 public class SplashActivity extends AppCompatActivity {
 
-    private static final int SPLASH_TIME = 1500;
+    private static final int SPLASH_TIME = 1000;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermissions();
+        } else {
+            startMainActivity();
+        }
+    }
+
+
+    private void startMainActivity() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -26,5 +43,43 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME);
+    }
+
+    /**
+     * Request permission on version 23 or above
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        } else {
+            startMainActivity();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startMainActivity();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Pozíció engedély szükséges az alkalmazás használatához", Toast.LENGTH_SHORT).show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            finish();
+                        }
+                    }, 500);
+                }
+            }
+        }
     }
 }
